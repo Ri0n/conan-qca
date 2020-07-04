b41c8e859227f2f421749b1b67d2f0ee4419ef6a
Sergey Ilinykh <rion4ik@gmail.com>
2020-07-04 09:38:14 +0300

    Attempt to fix appveyor #4

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
-from conan.packager import ConanMultiPackager
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(visual_runtimes=["MD", "MDd"])
+    builder = ConanMultiPackager(visual_runtimes=["MD", "MDd"], archs=['x86_64'])
     builder.add_common_builds(shared_option_name=False, pure_c=False)
     builder.run()
